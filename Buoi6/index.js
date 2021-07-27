4034373fe32c41367be6034257f5d5320ab1085b
codeinlife10 <codeinlife10@gmail.com>
2021-07-27 21:00:00 +0700

    BTVN buoi 7

--- Buoi6/index.js
+++ Buoi6/index.js
@@ -1,7 +1,17 @@
-for(let i = 0; i < 8;i++) {
-    if(i % 2 == 0) {
-        console.log(i,"là số chẵn")
-    }else {
-        console.log(i,"là số lẻ")
-    }
+// let input = [1,-8,5,7,2,-100,10]
+// let sum = 0
+
+
+// for(let i = 0;i<input.length;i++) {
+//     sum += input[i]
+// }
+
+var n = parseInt(prompt('nhập số phần tử muốn thêm vào:'));
+var arr =[1,2];
+
+for(var k=0;k<n;k++){
+ add = prompt('nhập phần tử muốn thêm:');
+  arr.push(add);
 }
+
+console.log(arr);
